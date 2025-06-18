67ab948e1449330a2be7110cb36dfd50439365ac
Kalen Young <kalenyoung03@gmail.com>
2025-06-18 11:08:29 +0200

    test

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'main-header',
+  selector: 'app-header',
   templateUrl: './header.component.html',
   standalone: true,
   styleUrls: ['./header.component.css']
