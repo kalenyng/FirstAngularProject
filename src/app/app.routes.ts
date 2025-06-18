afa473ab212247f993a3595ef39095f8e4dc4607
Kalen Young <kalenyoung03@gmail.com>
2025-06-18 19:15:28 +0200

    testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,11 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
-      import('./home/home.component').then(m => m.HomeComponent)
+      import('./home/home.component').then((m) => m.HomeComponent)
   },
   {
     path: 'about',
     loadComponent: () =>
-      import('./about/about.component').then(m => m.AboutComponent)
+      import('./about/about.component').then((m) => m.AboutComponent)
   }
 ];
